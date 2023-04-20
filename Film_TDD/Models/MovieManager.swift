8b1a24156cba81e6ae33bee0cc9fc10edaab0605
Christian.Starly <yoh.chrisitan@gmail.com>
2023-04-19 22:27:10 -0400

    create a test and helper method with builder pattern to create a set and movie to test

--- Film_TDD/Models/MovieManager.swift
+++ Film_TDD/Models/MovieManager.swift
@@ -8,6 +8,30 @@
 import Foundation
 
 struct MovieManager {
-    let moviesToSee: Int = 0
-    let moviesSeenCount: Int = 0
+
+    var moviesToSee: [Movie]
+    var moviesSeen: [Movie]
+
+    init(moviesSeen: [Movie] = [Movie](), moviesToSee: [Movie] = [Movie]()) {
+
+        self.moviesSeen = moviesSeen
+        self.moviesToSee = moviesToSee
+    }
+
+    mutating func add(_ movie: Movie) {
+
+        moviesToSee.append(movie)
+    }
+
+    func getMovie(at index: Int) -> Movie {
+
+        return moviesToSee[index]
+    }
+
+    mutating func checkOffMovie(at index: Int) {
+
+        guard index < moviesToSee.count else { return }
+        moviesSeen.append(moviesToSee[index])
+        moviesToSee.remove(at: index)
+    }
 }
--- Film_TDDTests/TestModels/MovieManagerTest.swift
+++ Film_TDDTests/TestModels/MovieManagerTest.swift
@@ -10,15 +10,50 @@ import XCTest
 
 class MovieManagerTest: XCTestCase {
 
+    //MARK: Initial values
     func test_init_moviewsToSeeReturnsZero() {
 
         let sut = MovieManager()
-        XCTAssertEqual(sut.moviesToSee, 0)
+        XCTAssertEqual(sut.moviesToSee.count, 0)
     }
 
     func test_init_moviesSeenReturnZero() {
 
         let sut = MovieManager()
-        XCTAssertEqual(sut.moviesSeenCount, 0)
+        XCTAssertEqual(sut.moviesSeen.count, 0)
+    }
+
+    //MARK: Add & Query
+    func test_add_moviesToSeeReturnsOne() {
+
+        var (sut, _) = makeSUT(title: "Sci-Fi")
+        XCTAssertEqual(sut.moviesToSee.count, 1)
+    }
+
+    func test_query_returnsMovieAtIndex() {
+
+        var (sut, movie) = makeSUT(title: "Arthouse Drama")
+        let movieQueried = sut.getMovie(at: 0)
+
+        XCTAssertEqual(movie.title, movieQueried.title)
+    }
+
+    //MARK: Checking Off
+    func test_checkOffMovie_updatesMovieManagerCount() {
+
+        var (sut, _) = makeSUT(title: "Action/Adventure")
+        sut.checkOffMovie(at: 0)
+
+        XCTAssertEqual(sut.moviesToSee.count, 0)
+        XCTAssertEqual(sut.moviesSeen.count, 1)
+    }
+
+    //MARK: Helpers
+    func makeSUT(title: String = "TestMovie") -> (sut: MovieManager, movie: Movie){
+
+        var sut = MovieManager()
+        let movie = Movie(title: title)
+        sut.add(movie)
+        return (sut, movie)
     }
 }
